f43acd6bf49b4b40eff71f579a0e814e43e0b6b4
Revati Patil <revati.patil@cavotec.com>
2025-06-26 11:00:28 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+import pandas as pd
+
+app = Flask(__name__)
+
+# Connect to the SQLite database
+engine = create_engine('sqlite:///imdb_data_exercise.db')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    movies = []
+    if request.method == 'POST':
+        search_ = request.form.get('content', ' ').strip()
+
+        query = """
+        SELECT Movie.title, Genre.name AS genre, Movie.year, Movie.rating, Country.name AS country, Person.name AS director
+        FROM Movie
+        JOIN M_Genre ON Movie.MID = M_Genre.MID
+        JOIN Genre ON M_Genre.GID = Genre.GID  
+        JOIN M_Country ON Movie.MID = M_Country.MID
+        JOIN Country ON M_Country.CID = Country.CID 
+        JOIN M_Director ON Movie.MID = M_Director.MID
+        JOIN Person ON M_Director.PID = Person.PID     
+        
+        WHERE Movie.title LIKE ?
+        OR Genre.name LIKE ?
+        OR Country.name LIKE ?
+        OR Person.name LIKE ?
+    
+        GROUP BY Movie.MID
+        ORDER BY Movie.rating DESC;
+        """
+        filters = [f"%{search_}%", f"%{search_}%", f"%{search_}%", f"%{search_}%"]
+        df = pd.read_sql_query(query, engine, params=(filters[0], filters[1], filters[2], filters[3]))
+        movies = df.to_dict(orient='records')
+        print(movies)
+    
+    return render_template('index.html', movies=movies)
+
+if __name__ == '__main__':
+    app.run(debug=True)
