08f3bfc229721bcfabcbce136211aee2e946d947
Revati Patil <revati.patil@cavotec.com>
2025-06-30 10:56:45 +0530

    made minor modifications to the ui

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from sqlalchemy import create_engine
 import pandas as pd
 
+
 app = Flask(__name__)
 
 # Connect to the SQLite database
@@ -11,11 +12,12 @@ engine = create_engine('sqlite:///imdb_data_exercise.db')
 def index(): 
     return render_template('index.html')
 
+# Route to handle the search form submission
 @app.route('/results', methods=['POST'])
 def results():
     movies = []
     if request.method == 'POST':
-        search_ = request.form.get('content', ' ').strip()
+        search_ = request.form.get('content', ' ').strip() # Default to a space if no input is provided and strip whitespace
 
         query = """
         SELECT Movie.title, Genre.name AS genre, Movie.year, Movie.rating, Country.name AS country, Person.name AS director, p.name AS actor
@@ -33,15 +35,18 @@ def results():
         OR Country.name LIKE ?
         OR Person.name LIKE ?
         OR p.name LIKE ?
-        GROUP BY Movie.MID
+        GROUP BY movie.mid
         ORDER BY Movie.rating DESC;
         """
-        filters = [f"%{search_}%", f"%{search_}%", f"%{search_}%", f"%{search_}%", f"%{search_}%"]
-        df = pd.read_sql_query(query, engine, params=(filters[0], filters[1], filters[2], filters[3], filters[4]))
+        # prepare the filters for the sql query
+        filters = [f"%{search_}%", f"%{search_}%", f"%{search_}%", f"%{search_}%", f"%{search_}%"] 
+        df = pd.read_sql_query(query, engine, params=(filters[0], filters[1], filters[2], filters[3], filters[4])) 
         movies = df.to_dict(orient='records')
         print(movies)
     
+    # render the results template with the movies data
     return render_template('results.html', movies=movies)
-
+ 
+# runs the app in debug mode
 if __name__ == '__main__':
     app.run(debug=True)
